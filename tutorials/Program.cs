f5efa3f8dca4a91849f0eb750077f0cd089ecb44
Marjolaine Tillet <123329559+marjolainett@users.noreply.github.com>
2026-03-27 13:29:08 +0000

    part 1: hello world

---
+++ tutorials/Program.cs
@@ -0,0 +1,28 @@
+﻿namespace tutorials;
+
+class Personne
+{
+    public Personne() : this("Anonymous", 0) { }
+    public Personne(string name, int age)
+    {
+        Name = name;
+        Age = age;
+    }
+    public string Name { get; set; }
+    public int Age { get; set; }
+    public string Hello(bool isLowercase)
+    {
+        var response = $"hello {Name}, you are {Age}";
+        if (isLowercase) return response;
+        else return response.ToUpper();
+    }
+}
+
+class Program
+{
+    static void Main()
+    {
+        var p = new Personne("Marjolaine", 23);
+        Console.WriteLine(p.Hello(true));
+    }
+}
