963f9880474e175dbdd95f9ee9f252f8c2268e82
Marjolaine Tillet <123329559+marjolainett@users.noreply.github.com>
2026-03-27 14:25:20 +0000

    part 3: resize images and measure parallel speedup

--- tutorials/Program.cs
+++ tutorials/Program.cs
@@ -2,6 +2,8 @@
 
 using System.Text.Json.Serialization;
 using Newtonsoft.Json;
+using SixLabors.ImageSharp;
+using SixLabors.ImageSharp.Processing;
 class Personne
 {
     public Personne() : this("Anonymous", 0) { }
@@ -24,7 +26,51 @@ class Program
 {
     static void Main()
     {
+        // Part 1 & 2
         var p = new Personne("Marjolaine", 23);
         Console.WriteLine(JsonConvert.SerializeObject(p, Formatting.Indented));
+
+        // Part 3
+        var nb_iterations = 10;
+        Console.WriteLine($"Comparison for {nb_iterations} iterations");
+
+        var watch = System.Diagnostics.Stopwatch.StartNew();
+        ResizeImgs(nb_iterations);
+        watch.Stop();
+        var elapsedMs = watch.ElapsedMilliseconds;
+        Console.WriteLine($"Sequential time: {elapsedMs}ms");
+
+        watch.Restart();
+        ResizeImgsWithParallel(nb_iterations);
+        watch.Stop();
+        var elapsedMsParallel = watch.ElapsedMilliseconds;
+        Console.WriteLine($"Parallel time: {elapsedMsParallel}ms");
+
+        Console.WriteLine($"Speedup: {(double)elapsedMs / elapsedMsParallel:F2}");
     }
+
+    private static void ResizeImgs(int nb_iterations)
+    {
+        for (int i = 0; i < nb_iterations; i++) ResizeImg(i);
+    }
+
+    private static void ResizeImgsWithParallel(int nb_iterations)
+    {
+        Parallel.For(0, nb_iterations, i => ResizeImg(i));
+    }
+
+    private static void ResizeImg(int i)
+    {
+        string inPath = $@"img/canard_{i}.png";
+        string outPath = $@"img/resized/canard_{i}_resized.png";
+        using (Image image = Image.Load(inPath))
+        {
+            int width = image.Width / 2;
+            int height = image.Height / 2;
+            image.Mutate(x => x.Resize(width, height));
+
+            image.Save(outPath);
+        }
+    }
+
 }
